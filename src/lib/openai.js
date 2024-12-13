1c45c6281e8465622b031edc3826c70a6e805915
ayu <ayuabc4528@gmail.com>
2024-12-13 09:48:20 +0900

    OpenAI APIを叩くためのクラスを作成

---
+++ src/lib/openai.js
@@ -0,0 +1,30 @@
+const { default: axios } = require("axios");
+
+class OpenaiClient {
+	constructor() {
+		this.apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+	}
+
+	async completion(messages) {
+		const requestData = {
+			model: "gpt-4o-mini",
+			messages,
+		};
+
+		const response = await axios.post(
+			"https://api.openai.com/v1/chat/completions",
+			requestData,
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${this.apiKey}`
+				},
+			}
+		);
+
+		return response.data.choices[0].message.content;
+	}
+}
+
+const openai = new OpenaiClient();
+export default openai;
